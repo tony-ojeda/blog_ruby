cb144a3c24b84cf3cae656dd883be291c861d7fb
tony-ojeda <tojeda96@gmail.com>
2020-05-21 21:07:23 -0500

    feature: editing image for each article

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,6 +5,23 @@ class ImagesController < ApplicationController
         @image = @article.images.build
     end
 
+    def edit
+        @article = Article.find(params[:article_id])
+        @image = @article.images.find(params[:id])
+    end
+
+    def update 
+        @article = Article.find(params[:article_id])
+        @image = @article.images.find(params[:id])
+        image = params[:image][:image]
+        
+        if @image.update_attributes(image: Base64.encode64(image.read))
+            redirect_to @article
+        else
+            render action: "edit"
+        end
+    end
+
     def create
         @article = Article.find(params[:article_id])
         @image = @article.images.build(image_params)
---
+++ spec/features/editing_image_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'base64'
+
+RSpec.feature "Admins pueden editar imágenes" do
+    scenario "con atributos válidos imágenes" do
+        article_1 = Article.create(name: "Razones porlas cuales aprender Ruby on Rails",
+        content: "Es un gran lenguaje de programación")
+        image = File.new("spec/images/2.jpg")
+        image_base_64 = "data:image/jpeg;base64," + Base64.encode64(image.read)
+        image_1 = Image.create(image: image_base_64, article_id: article_1.id )
+
+        edited_image = File.new('spec/images/3.jpg')
+        edited_image_base_64 = "data:image/jpeg;base64," + Base64.encode64(edited_image.read)
+
+        visit "/"
+        click_link article_1.name
+        click_link "Cambiar Imagen"
+        attach_file "Image","spec/images/3.jpg"
+        click_button "Guardar"
+
+        expect(page).to have_xpath("//img[@src='"+edited_image_base_64+"']")
+    end
+end
